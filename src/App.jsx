66eed882af96f44b4d94dd3df00a9da070c3fbd9
arun <arunpandian97@gmail.com>
2023-08-27 17:28:56 +0530

    deleted some file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Create from './components/create'
-import './App.css'
 import Home from './components/home'
 import { Routes,Route } from 'react-router-dom'
 import Edit from './components/edit'
